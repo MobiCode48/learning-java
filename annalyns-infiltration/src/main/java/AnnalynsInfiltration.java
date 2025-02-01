1fce4faa46972ce1aeabf06a38a530e2dd02adb9
MobiCode48 <emmanuelmanu544@gmail.com>
2025-02-01 18:47:50 +0100

    annalyn exercise done - boolean

---
+++ annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -0,0 +1,20 @@
+class AnnalynsInfiltration {
+    public static boolean canFastAttack(boolean knightIsAwake) {
+        return !knightIsAwake;
+    }
+
+    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
+    }
+
+    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+        return prisonerIsAwake && !archerIsAwake;
+    }
+
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake,
+            boolean petDogIsPresent) {
+        boolean has_her_dog = petDogIsPresent && !archerIsAwake;
+        boolean no_dog = !petDogIsPresent && (prisonerIsAwake && (!knightIsAwake && !archerIsAwake));
+        return has_her_dog || no_dog;
+    }
+}
